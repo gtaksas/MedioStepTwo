06f81733311d6f90265d3c4216e5310225cc4b7f
gtaksas <gtaksas@gmail.com>
2020-11-16 21:29:29 +0100

    Creating PageTwo. It has an image and will display another one when first page s onPress event called

---
+++ PageTwo.js
@@ -0,0 +1,37 @@
+﻿import React, { Component } from 'react';
+import { StyleSheet, Image, Alert} from 'react-native';
+
+
+
+export default class PageTwo extends Component {
+
+    render() {
+
+        if (this.props.check) {
+            return (
+                <Image
+                    style={styles.bgPicture}
+                    source={{ uri: this.props.check }}
+                />
+            );
+        }
+
+        else {
+            return (
+                <Image
+                    style={styles.bgPicture}
+                    source={{ uri: 'https://image.freepik.com/free-photo/medical-doctor-woman-blue-background-holding-stethoscope-focus_136686-6.jpg' }}
+                />
+            );
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    bgPicture: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover',
+    }
+});
